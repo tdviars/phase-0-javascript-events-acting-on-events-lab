937491a0c93fe1673722ce87ff0c7e0888ec96c6
taylor <taylor.viars@yahoo.com>
2022-11-20 14:35:26 -0500

    commit tree a12ff07d207ecd8213e2739a9de99307ded80352

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
